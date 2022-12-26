d1d08a11d6d6ab2dc823b31cf6ef10adecbfad2f
Emir Hurturk <emirhurturk444@gmail.com>
2022-12-27 01:05:03 +0300

    model construction

--- src/model.py
+++ src/model.py
@@ -7,20 +7,35 @@ classes = [
     "ship", "truck"
 ]
 
+classes_lung = ["NORMAL", "BENIGN", "MALIGNANT"]
+
 
 def main():
     # use the model that is in models/cifar10
-    model = tf.keras.models.load_model("models/cifar10/")
+    model = tf.keras.models.load_model("../models/lung/")
     # load example image
     # img = load_img("src/assets/plane.jpeg")
     # # FIXME: Image dimensions are 185x275, however, the model expects 32, 32 images.
     # img = np.reshape(img, (32, 32, 3))
     # img = img_to_array(img)[:, :, :1]  # grayscale
-    img = cv2.imread("src/assets/bird.jpeg")
-    img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_AREA)
+    img = cv2.imread("assets/benignlung.jpeg")
+    if img is None:
+        print("Couldn't read the image.")
+    img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_AREA)
+    cv2.imshow("zaza", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
+    img2 = cv2.imread(
+        "assets/datasets/lungIQ/malignant/Malignant case (2).jpg")
+    img2 = cv2.resize(img2, (64, 64), interpolation=cv2.INTER_AREA)
+    cv2.imshow("zaza", img2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
     img = np.expand_dims(img, axis=0)  # 32x32x3 = ?x
-    Y_pred = model.predict(img)[0]
+    Y_pred = model.predict(img)
+    print(Y_pred)
+    Y_pred = Y_pred[0]
     idx = 0
     max_val = 0
     for i in range(0, len(Y_pred)):
@@ -28,7 +43,7 @@ def main():
             idx = i
             max_val = Y_pred[i]
 
-    print(classes[idx])
+    print(classes_lung[idx])
 
 
 if __name__ == "__main__":
